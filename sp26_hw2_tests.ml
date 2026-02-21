6cdf912f38221d812b6c74396951e8be9d6d4bd2
Grace Sun <gs0625@princeton.edu>
2026-02-21 21:11:56 +0000

    cleanup

--- sp26_hw2_tests.ml
+++ sp26_hw2_tests.ml
@@ -10,10 +10,7 @@ let ex_tests_with_path =
      - an LLVM IR file
      - the expected return code
      Note that return codes are truncated to one byte. *)
-  [ "sp26_hw2_tests/ntz.ll", 6L
-  ; even_odd: no arg -> invalid -> -1 (truncated to 255)
-  "sp26_hw2_tests/even_odd.ll", 255L
-  ]
+  [ "sp26_hw2_tests/ntz.ll", 6L]
 
 let io_tests_with_path =
   (* Format: each io test is a triple consisting of
